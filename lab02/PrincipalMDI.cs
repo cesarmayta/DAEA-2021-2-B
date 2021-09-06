a603d277d9bb37053158e781666bc02cb836dbac
Cesar Mayta <cesarmayta@gmail.com>
2021-09-06 13:46:29 -0500

    lab02 final

---
+++ lab02/PrincipalMDI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab02
+{
+    public partial class PrincipalMDI : Form
+    {
+        public PrincipalMDI()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuSisSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mnuManUsuarios_Click(object sender, EventArgs e)
+        {
+            manUsuario frm = new manUsuario();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuManProveedores_Click(object sender, EventArgs e)
+        {
+            manProveedores frmProveedores = new manProveedores();
+            frmProveedores.MdiParent = this;
+            frmProveedores.Show();
+        }
+    }
+}
